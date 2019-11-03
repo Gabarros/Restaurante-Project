de765c6cc980163e06ade79b40daab5f01d6e7e7
Gabarros <gabarros17@gmail.com>
2019-11-02 22:39:34 -0300

    Add Include Templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(`
+    SELECT * FROM tb_menus ORDER BY title
+  `, (err, results)=>{
+
+    if(err){
+      console.error(err);
+    }
+
+    res.render('index', { 
+      title: 'Restaurante Saboroso!',
+      menus: results
+
+     });
+
+  })
+  
+});
+
+router.get('/contacts', function(req, res, next){
+
+  res.render('contacts', {
+    title: 'Contatos - Restaurante Saboroso',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um Oi!'
+  });
+
+});
+
+router.get('/menus', function(req, res, next){
+
+  res.render('menus', {
+    title: 'Menus - Restaurante Saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
+  });
+  
+});
+
+router.get('/services', function(req, res, next){
+
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
+  });
+  
+});
+router.get('/reservations', function(req, res, next){
+
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Saboroso',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
+  });
+  
 });
 
 module.exports = router;
+
+
